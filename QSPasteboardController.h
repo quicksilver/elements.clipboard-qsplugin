cd1c848de6be3e830a2801f30f0b0005dffb7641
Rob McBroom <github@skurfer.com>
2014-05-06 09:26:16 -0400

    reverse the behavior of the "store on disk" pref
    This will allow it to be disabled for new users by default, but not
    interfere with the behavior for existing users.

--- QSPasteboardController.h
+++ QSPasteboardController.h
@@ -12,7 +12,7 @@ typedef enum {
 
 #define kCapturePasteboardHistory @"Capture Pasteboard History"
 #define kCapturePasteboardHistoryCount @"Capture Pasteboard History Count"
-#define kStorePasteboardHistoryOnDisk @"Store Pasteboard History"
+#define kDiscardPasteboardHistoryOnRelaunch @"Discard Pasteboard History"
 #define kCapturePasteboardIgnoreApps @"clipboardIgnoreApps"
 
 @interface QSPasteboardController : NSWindowController {
